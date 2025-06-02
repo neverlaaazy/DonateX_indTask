5b2d6cdbae510dbdd18ad343b922a9a5b6360118
neverlaaazy <cheserega174@gmail.com>
2025-06-02 18:47:26 +0500

    preFinishLine

--- js/script.js
+++ js/script.js
@@ -1,15 +1,21 @@
 const btnOpen = document.querySelector(".HMenu_button");
 const btnOpen2 = document.querySelector(".OCcard_button");
+const btnOpen3 = document.querySelector(".OCcard_but2");
+const btnOpen4 = document.querySelector(".OCcard_but3");
 const modal = document.querySelector(".modal");
 const btnModal = document.querySelector(".modal__button");
 
 const modalViewHandler = () =>{
+    modal.style.display = 'flex';
     modal.classList.toggle('modal--open');
 }
 const modalDeleteHandler = () =>{
     modal.classList.remove('modal--open');
+    modal.style.display = 'none';
 }
 
 btnOpen.addEventListener('click',modalViewHandler);
 btnOpen2.addEventListener('click',modalViewHandler);
+btnOpen3.addEventListener('click',modalViewHandler);
+btnOpen4.addEventListener('click',modalViewHandler);
 modal.addEventListener('click',modalDeleteHandler);
